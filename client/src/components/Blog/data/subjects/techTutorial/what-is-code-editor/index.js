bfeeaf58957f4f578c7fe9810f6e930d6d4c7931
Khushdil380 <khushdilansari345@gmail.com>
2026-01-14 02:54:36 +0530

    extra file deleted

--- client/src/components/Blog/data/subjects/techTutorial/what-is-code-editor/index.js
+++
@@ -1,8 +0,0 @@
-import { createTopic } from "../../contentParser";
-import content from "./what-is-code-editor.content";
-
-export default createTopic(
-  "what-is-code-editor",
-  "What is Code Editor",
-  content
-);
